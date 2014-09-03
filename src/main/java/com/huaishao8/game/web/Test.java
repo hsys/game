6df5804eba3a55725fd6bc86557c94fe9a931378
hsys <yangpingzhun@163.com>
2014-09-03 23:01:01 +0800

    Test

---
+++ src/main/java/com/huaishao8/game/web/Test.java
@@ -0,0 +1,11 @@
+package com.huaishao8.game.web;
+
+/**
+ * Created by ypz on 2014/9/3.
+ */
+public class Test {
+
+    public static void main(String[] agas){
+        System.out.println("Hello world !");
+    }
+}
